eae7107b9db2196d71054bccbe32930e9f2f491a
Prithviraj Sawant <prithvirajsawant15@example.com>
2023-06-23 20:59:21 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,95 @@
+'use strict';
+
+
+
+/**
+ * navbar toggle
+ */
+
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("nav-active");
+  this.classList.toggle("active");
+});
+
+/**
+ * toggle the navbar when click any navbar link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    header.classList.toggle("nav-active");
+    navToggleBtn.classList.toggle("active");
+  });
+}
+
+
+
+
+
+/**
+ * back to top & header
+ */
+
+const backTopBtn = document.querySelector("[data-back-to-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+/* 
+ *Parallax Effect
+ */ 
+ const parallaxElements = document.querySelectorAll("[data-parallax]");
+
+ window.addEventListener("mousemove", event =>{
+     for(let i=0, len=parallaxElements.length; i<len; i++){
+         const movementX = (event.clientX/window.innerWidth)*Number(parallaxElements[i].dataset.parallaxSpeed);
+         const movementY = (event.clientY/window.innerHeight)*Number(parallaxElements[i].dataset.parallaxSpeed);
+ 
+         parallaxElements[i].animate({
+             transform: `translate(${movementX}px, ${movementY}px)`
+         }, {duration: 500, fill: "forwards"} );
+     }
+ })
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });
+
